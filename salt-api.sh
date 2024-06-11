1c5a401c1e211d66898f5105200a9a2bc5341c38
Mauricio R. Morales R. <mauricio1999morales@gmail.com>
2024-06-11 14:28:30 -0400

    Test CI

---
+++ salt-api.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define variables
+API_URL="http://localhost:8000"
+USERNAME="saltuser"
+PASSWORD="pan"
+EAUTH="pam"
+
+# Obtain auth token
+TOKEN=$(curl -sSk $API_URL/login -d username=$USERNAME -d password=$PASSWORD -d eauth=$EAUTH | jq -r '.return[0].token')
+
+# Execute a command
+RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='local' -d tgt='*' -d fun='state.apply' -d arg='states.events.file_create')
+
+# Display response
+echo $RESPONSE
