4fd37750c835c3efaa61a88c9e8b8443cdd856be
Mauricio R. Morales R. <mauricio1999morales@gmail.com>
2024-06-12 19:08:30 -0400

    Update salt-api.sh

--- salt-api.sh
+++ salt-api.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Define variables
-API_URL="http://192.168.0.113:8000"
+API_URL="http://3.130.220.130:8000"
 USERNAME="saltuser"
-PASSWORD="pan"
-EAUTH="pam"
+PASSWORD="koshee"
+EAUTH="auto"
 
 # Obtain auth token
 TOKEN=$(curl -sSk $API_URL/login -d username=$USERNAME -d password=$PASSWORD -d eauth=$EAUTH | jq -r '.return[0].token')
 
 # Execute a command
-RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='local' -d tgt='*' -d fun='state.apply' -d arg='states.events.file_create')
+RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='local' -d tgt='koshee-syndic-master' -d fun='state.apply' -d arg='states.events.file_create')
 
 # Display response
 echo $RESPONSE
