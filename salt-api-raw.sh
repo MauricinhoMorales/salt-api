54b63da47cb1c8b60ab98251eb4a47cfbcacfbce
Mauricio R. Morales R. <mauricio1999morales@gmail.com>
2024-06-13 20:03:24 -0400

    Create salt-api-raw.sh

---
+++ salt-api-raw.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Define variables
+API_URL="http://3.130.220.130:8000"
+USERNAME="koshee"
+PASSWORD="koshee"
+EAUTH="auto"
+
+# Obtain auth token
+TOKEN=$(curl -sSk $API_URL/login -d username=$USERNAME -d password=$PASSWORD -d eauth=$EAUTH | jq -r '.return[0].token')
+
+# Execute a command
+RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='local' -d tgt='koshee-syndic-master' -d fun='state.apply' -d arg='states.events.file_create')
+
+# Display response
+echo $RESPONSE
