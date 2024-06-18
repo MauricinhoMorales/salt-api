2594ffafeddfd1d7702087f470aaf11143b0efcb
Mauricio R. Morales R. <mauricio1999morales@gmail.com>
2024-06-17 20:10:32 -0400

    Update salt-api.sh

--- salt/salt-api.sh
+++ salt/salt-api.sh
@@ -10,7 +10,7 @@ EAUTH="auto"
 TOKEN=$(curl -sSk $API_URL/login -d username=$USERNAME -d password=$PASSWORD -d eauth=$EAUTH | jq -r '.return[0].token')
 
 # Execute a command
-RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='local' -d tgt='koshee-syndic-master' -d fun='state.apply' -d arg='states.events.file_create'| jq .)
+RESPONSE=$(curl -sSk $API_URL -H "X-Auth-Token: $TOKEN" -d client='runner' -d fun='state.orchestrate' -d arg='orchestrate.orchestrate_ci' -d arg='pillar={"minion_id":"CoreCluster12"}'| jq .)
 
 # Display response
 echo $RESPONSE
