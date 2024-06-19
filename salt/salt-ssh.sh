d5c99a68557474b72d98f4447ea44b89fa882891
Mauricio R. Morales R. <mauricio1999morales@gmail.com>
2024-06-19 18:03:29 -0400

    test

--- salt/salt-ssh.sh
+++ salt/salt-ssh.sh
@@ -6,7 +6,7 @@ SALT_MASTER_USER=$SALT_MASTER_USER
 SSH_PRIVATE_KEY="~/.ssh/ci_key_koshee.pem"
 
 # Define the orchestrate command to be executed on the Salt master
-ORCHESTRATE_COMMAND="sudo salt-run state.orchestrate orchestrate.orchestrate_ci pillar='{\"targets\":\"CoreCluster1 or CoreCluster12\"}'"
+ORCHESTRATE_COMMAND="echo "koshee" | sudo -S salt-run state.orchestrate orchestrate.orchestrate_ci pillar='{\"targets\":\"CoreCluster1 or CoreCluster12\"}'"
 
 # Execute the command via SSH
 RESPONSE=$(ssh -i "$SSH_PRIVATE_KEY" "$SALT_MASTER_USER@$SALT_MASTER_HOST" "$ORCHESTRATE_COMMAND")
